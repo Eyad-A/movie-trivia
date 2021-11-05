10cf8056fbe70b8378ef072d0fffb72ced9d69ce
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-11-05 10:24:48 -0500

    Add main functions to App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 function App() {
+  
+  async function startQuiz() {
+
+  }
+
+  function checkAnswer(e: React.MouseEvent<HTMLButtonElement>) {
+
+  }
+
+  function nextQuestion() {
+
+  }
+  
   return (
     <div className="App">
-      Matrix Trivia 
+      <h1>Movie Trivia </h1>
+      <button className="start" onClick={startQuiz}>
+        Start quiz
+      </button>
+      <p className="score">Score: </p>
+      <p>Loading. Please wait...</p> 
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question 
+      </button>
     </div>
   );
 }
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function QuestionCard() {
+  return (
+    <div>
+      Question Card
+    </div>
+  )
+
+}
+
+export default QuestionCard;
