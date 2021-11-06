a66c0323b8d31b1a2129372227bc4bfe6ee76769
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-11-06 01:51:40 -0500

    Add states and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import QuestionCard from './components/QuestionCard';
 
+const TOTAL_QUESTIONS = 10;
+
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
   
   async function startQuiz() {
 
@@ -23,7 +31,14 @@ function App() {
       </button>
       <p className="score">Score: </p>
       <p>Loading. Please wait...</p> 
-      <QuestionCard />
+      <QuestionCard 
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question 
       </button>
